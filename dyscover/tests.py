588cb6e5cf20d1d06ba1ffa65a3af20dc6da0c5e
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-13 20:07:25 +0530

    Update tests

--- dyscover/tests.py
+++ dyscover/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 
 class DyscoverViewsTestCase(TestCase):
-    def test_index(self):
+    def test_correct_query(self):
         resp = self.client.get('/api/search/', {'q':'delhi'})
         self.assertEqual(resp.status_code, 200)
+    
+    
+    def test_incorrect_parameter(self):
         resp = self.client.get('/api/search/', {'x':'delhi'})
         self.assertEqual(resp.status_code, 400)
+    
+    
+    def test_invalid_data(self):
         resp = self.client.get('/api/search/', {'q':'lmklaaoq'})
         self.assertEqual(resp.status_code, 404)
