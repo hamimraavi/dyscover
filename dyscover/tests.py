b2d5d1d15c1eb337189f6cbabf4e98361a140178
Hamim Raavi <hamim.raavi@grofers.com>
2016-06-13 19:42:39 +0530

    Test 1: Multiple http codes

--- dyscover/tests.py
+++ dyscover/tests.py
@@ -2,5 +2,9 @@ from django.test import TestCase
 
 class DyscoverViewsTestCase(TestCase):
     def test_index(self):
-        resp = self.client.get('/api/search/?q=delhi')
+        resp = self.client.get('/api/search/', {'q':'delhi'})
         self.assertEqual(resp.status_code, 200)
+        resp = self.client.get('/api/search/', {'x':'delhi'})
+        self.assertEqual(resp.status_code, 400)
+        resp = self.client.get('/api/search/', {'q':'lmklaaoq'})
+        self.assertEqual(resp.status_code, 404)
